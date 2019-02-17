f7d834708b82f4e43026d0d23663f13d72952d08
heidiulery <heidiulery@hotmail.com>
2019-02-16 21:59:45 -0600

    Create chat rooms checkpoint: completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,10 +6,15 @@ class RoomList extends Component {
 		super(props);
 
 		this.state = {
-			rooms: []
+			rooms: [],
+			name: '',
+			isHidden: true
 		};
 
 		this.roomsRef = this.props.firebase.database().ref('rooms');
+		this.handleChange = this.handleChange.bind(this);
+    	this.createRoom = this.createRoom.bind(this);
+		this.toggleHidden = this.toggleHidden.bind(this);
 	}
 
 	componentDidMount() {
@@ -20,17 +25,52 @@ class RoomList extends Component {
 		});
 	}
 	
+	createRoom(e) {
+		e.preventDefault();
+		const newRoomName = this.state.name;
+		if (newRoomName.length >= 1) {
+			this.roomsRef.push({ name: newRoomName });
+		}
+		this.setState({ name: '' });
+		this.setState({ isHidden: true });
+	}
+
+	handleChange(e) {
+		this.setState({ name: e.target.value });
+	}
+
+	toggleHidden() {
+		this.setState({ isHidden: !this.state.isHidden })
+	}
+
 	render() {
+		const roomList = this.state.rooms.map( room =>
+			<li key={ room.key }>{ room.name }</li> 
+		);
+
+		const roomForm = (
+			<form onSubmit={ this.createRoom }>
+				<h1>Create new room</h1>
+				<input
+					className="inputStyle"
+					type="text" 
+					value={ this.state.name } 
+					placeholder="Enter a room name"
+					onChange={ this.handleChange }
+				/>
+				<div>
+					<button className="create" onSubmit={ this.createRoom }>Create</button>
+				</div>
+			</form>
+		);
+
 		return (
-			<section className='roomList'>
-				<table><tbody>
-					<h1>Bloc Chat</h1>
-					{
-						this.state.rooms.map( (room, index) =>
-							<tr key={ index }>{ room.name }</tr>
-						)
-					}
-				</tbody></table>
+			<section className="roomList">
+				<img src={require("../wordbubble.jpg")} alt="Bloc Chat" className="blocChatLogo" />
+				<div className="listTitle">Available chat rooms:</div>
+				<ul>{ roomList }</ul>
+				<button className="addRoom" onClick={ this.toggleHidden }>Create new room</button>
+				{ !this.state.isHidden && roomForm }
 			</section>
 		)
 	}
